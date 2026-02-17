dd74225a77020b6c3d57f8a61c24db3876f19d57
jokeryak <errorwork9@gmail.com>
2026-02-17 21:32:11 +0600

    Initial Ramadan 2026 countdown site

---
+++ script.js
@@ -0,0 +1,23 @@
+const ramadanDate = new Date("March 18, 2026 00:00:00").getTime();
+
+const timer = setInterval(function () {
+
+  const now = new Date().getTime();
+  const distance = ramadanDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.querySelector(".countdown").innerHTML = "Ramadan Mubarak!";
+  }
+
+}, 1000);
